22d56ad69fd17c4d0b4b0d4bbab59d8f17df2473
hagevvashi <hagevvashi@gmail.com>
2020-12-23 14:42:13 +0900

    feat: ABLC ac ans

---
+++ ABLC/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+
+int n;
+int * data;
+
+void init() {
+  // child: parent's index
+  // root: -size
+  data = array(n, int);
+
+  rep(i, n) {
+    // at first, all data is root and size is -1
+    data[i] = -1;
+  }
+}
+
+int find(int x) {
+  if (data[x] < 0) {
+    return x;
+  }
+
+  return data[x] = find(data[x]);
+}
+
+int same(int x, int y) {
+  return find(x) == find(y);
+}
+
+int unite(int x, int y) {
+  x = find(x);
+  y = find(y);
+  if (x == y) {
+    return 0;
+  }
+  // becasue data has minus size, reverse the inequality sign
+  if (data[x] > data[y]) {
+    swap(int, x, y);
+  }
+  data[x] += data[y];
+  data[y] = x;
+  return 1;
+}
+
+int size(int x) {
+  return -data[find(x)];
+}
+
+int count() {
+  int cnt = 0;
+  rep(i, n) {
+    if (find(i) == i) {
+      cnt += 1;
+    }
+  }
+  return cnt;
+}
+
+int main() {
+  int m;
+  scanf("%d%d",&n,&m);
+
+  // initialize
+  init();
+
+  rep(i,m){
+    int a,b;
+    scanf("%d%d", &a, &b);
+    unite(a-1,b-1);
+  }
+
+  printf("%d\n", count() - 1);
+  free(data);
+  return 0;
+}
