ad141bb8459d65568df1b6ed6ea184fafda6ebc3
hagevvashi <hagevvashi@gmail.com>
2020-12-30 00:16:57 +0900

    refactor: NTL_1_A: Prime Factorize
    Originally, it was a list of prime numbers, but I refactored it by defining a structure with prime numbers and their exponents.

--- 20201229_NTL_1_A/main.c
+++ 20201229_NTL_1_A/main.c
@@ -11,27 +11,62 @@
 #define swap(a, b) { int temp = a; a = b; b = temp; }
 #define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
-int ans[100010]={0};
+int n;
+
+typedef struct _Prime {
+  int body;
+  int shisu;
+} Prime;
+
+Prime prime_list[1010]={{0,0}};
 int cnt=0;
+
+void g(int x){
+  if(cnt==0){
+    prime_list[cnt].body=x;
+    prime_list[cnt].shisu+=1;
+    cnt+=1;
+  }else{
+    if(prime_list[cnt-1].body==x){
+      prime_list[cnt-1].shisu+=1;
+    }else{
+      prime_list[cnt].body=x;
+      prime_list[cnt].shisu+=1;
+      cnt+=1;
+    }
+  }
+}
 
 void f(int x){
   for(int i=2;i*i<=x;i+=1){
     if(x%i==0){
-      ans[cnt]=i;
-      cnt+=1;
+      g(i);
       return f(x/i);
     }
   }
-  ans[cnt]=x;
-  cnt+=1;
+  g(x);
+}
+
+void print(){
+  printf("%d:",n);
+  rep(i,cnt){
+    rep(j,prime_list[i].shisu){
+      printf(" %d",prime_list[i].body);
+    }
+  }
+  puts("");
 }
 
 int main() {
-  int n;
   scanf("%d",&n);
   f(n);
-  printf("%d:",n);
-  rep(i,cnt)printf(" %d",ans[i]);
-  puts("");
+
+  /* printf("cnt: %d\n",cnt); */
+  /* rep(i,cnt){ */
+  /*   printf("prime_list[i].body: %d\n",prime_list[i].body); */
+  /*   printf("prime_list[i].shisu: %d\n",prime_list[i].shisu); */
+  /* } */
+
+  print();
   return 0;
 }
