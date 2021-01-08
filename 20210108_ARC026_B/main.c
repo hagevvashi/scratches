1aa78d0fb941f4793417601726cdba5e560e4f13
hagevvashi <hagevvashi@gmail.com>
2021-01-08 23:30:46 +0900

    feat: add ARC026_B

---
+++ 20210108_ARC026_B/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+typedef struct {
+  ll prime_number;
+  int exponent;
+} Factor;
+
+typedef struct {
+  Factor list[11010];
+  int count;
+} Factors;
+
+void increment_exponent(int i,Factors*factors){
+  factors->list[i].exponent+=1;
+}
+
+void add_factor(ll x,Factors*factors){
+  factors->list[factors->count].prime_number=x;
+  increment_exponent(factors->count,factors);
+  factors->count+=1;
+}
+
+void add_or_increment(ll x,Factors*factors){
+  rep(i,factors->count){
+    if(factors->list[i].prime_number==x){
+      return increment_exponent(i,factors);
+    }
+  }
+  add_factor(x,factors);
+}
+
+void find_smallest_prime_factor(ll x,Factors*factors){
+  if(x<=1)return;
+  for(ll i=2;i*i<=x;i+=1){
+    if(x%i==0){
+      add_or_increment(i,factors);
+      return find_smallest_prime_factor(x/i,factors);
+    }
+  }
+  add_or_increment(x,factors);
+}
+
+Factors*find_prime_factors(ll x){
+  Factors*factors=malloc(sizeof(Factors));
+  factors->count=0;
+  find_smallest_prime_factor(x,factors);
+  return factors;
+}
+
+void debug(Factors*factors){
+  rep(i,factors->count){
+    Factor factor=factors->list[i];
+    printf("%lld to the power %d\n",factor.prime_number,factor.exponent);
+  }
+}
+
+ll mypow(ll base,int exponent){
+  ll r=1;
+  if(exponent==0)return r;
+  rep(i,exponent)r*=base;
+  return r;
+}
+
+ll divisor_sum(ll x){
+  ll r=1;
+  Factors*factors=find_prime_factors(x);
+  // debug(factors);
+  rep(i,factors->count){
+    ll sum=0;
+    Factor factor=factors->list[i];
+    rep(j,factor.exponent+1){
+      sum+=mypow(factor.prime_number,j);
+    }
+    r*=sum;
+  }
+  free(factors);
+  return r;
+}
+
+int main() {
+  ll n;
+  scanf("%lld",&n);
+  ll sum=divisor_sum(n);
+  if((n*2)==sum){
+    puts("Perfect");
+  }else{
+    if((n*2)>sum){
+      puts("Deficient");
+    }else{
+      puts("Abundant");
+    }
+  }
+  return 0;
+}
