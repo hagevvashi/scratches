c6b37cd2f9095122e17f658a54f216f4fde24e17
hagevvashi <hagevvashi@gmail.com>
2021-01-21 10:01:41 +0900

    feat: add ABC118-C ac ans

---
+++ 20210121_ABC118_C/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+ll gcd(ll a,ll b){
+  if(b==0)return a;
+  return gcd(b,a%b);
+}
+
+int main() {
+  int n;
+  scanf("%d",&n);
+  ll a[n];
+  rep(i,n)scanf("%lld",a+i);
+  ll ans=0;
+  rep(i,n)ans=gcd(ans,a[i]);
+  printf("%lld\n",ans);
+  return 0;
+}
