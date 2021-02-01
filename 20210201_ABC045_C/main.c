48c573a83c09d83a9ef4900169acaeec81dbcbb2
hagevvashi <hagevvashi@gmail.com>
2021-02-01 22:07:52 +0900

    wiop

---
+++ 20210201_ABC045_C/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main() {
+  char s[11];
+  scanf("%s",s);
+
+  int l=0;
+  while(1){
+    if(s[l]=='\0'){
+      break;
+    }
+    l+=1;
+  }
+
+  rep(bit,1<<(l-1)){
+  }
+  return 0;
+}
