8572f2e9f2472cf0415872d7ca1a9605dcaaf5ca
hagevvashi <hagevvashi@gmail.com>
2021-02-05 22:05:13 +0900

    feat: add ABC045 C ac ans

---
+++ 20210205_ABC045_C/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int stl(const char*s){
+  int l=0;
+  while(s[l]!='\0')l+=1;
+  return l;
+}
+
+int to_int(const char c){
+  return c-'0';
+}
+
+int main() {
+  char s[11];
+  scanf("%s",s);
+  int l=stl(s);
+  ll ans=0;
+  rep(bit,1<<(l-1)){
+    ll t=to_int(s[0]);
+    rep(i,l-1){
+      if((bit>>i)&1){
+        ans+=t;
+        t=to_int(s[i+1]);
+      }else{
+        t*=10;
+        t+=to_int(s[i+1]);
+      }
+    }
+    ans+=t;
+  }
+  printf("%lld\n",ans);
+  return 0;
+}
