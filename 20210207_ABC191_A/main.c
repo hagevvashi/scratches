fb504444d3b779a811ec7adca55075acdab3abb1
hagevvashi <hagevvashi@gmail.com>
2021-02-07 19:59:21 +0900

    feat: add ABC191 ac ans

---
+++ 20210207_ABC191_A/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main() {
+  int v,t,s,d;
+  scanf("%d%d%d%d",&v,&t,&s,&d);
+  int vt=v*t,vs=v*s;
+  if(vt<=d&&vs>=d){
+    puts("No");
+  }else{
+    puts("Yes");
+  }
+  return 0;
+}
